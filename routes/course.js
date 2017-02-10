4e354cf96799251632225d126940c39f0cc448e3
Heitor Schueroff <h.schueroff@gmail.com>
2017-02-09 19:27:40 -0800

    Skeleton code done

--- routes/course.js
+++ routes/course.js
@@ -1,3 +1,18 @@
+var courses = require('../data.json').courses;
+
 exports.show = function(req, res) {
-  res.render('course');
+  var len = courses.length;
+  var course;
+  for (var i = 0; i < len; i++) {
+    if (courses[i].id === req.params.id) {
+      course = courses[i];
+      break;
+    }
+  }
+
+  if (course) {
+    res.render('course', course);
+  } else {
+    res.render('course');
+  }
 };
